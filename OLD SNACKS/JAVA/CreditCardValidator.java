c82799b28f5d9cf0b8fd41d849ef021892532039
Republic Oba <republicoba1@gmail.com>
2025-06-20 15:16:47 +0200

    work

--- OLD SNACKS/JAVA/CreditCardValidator.java
+++ OLD SNACKS/JAVA/CreditCardValidator.java
@@ -12,8 +12,8 @@ public class CreditCardValidator{
 		int oddSum = 0;
 		boolean isEvenPosition = false;
 
-		for (int i = cardNumber.length - 1; i > 0; i++){	
-			int digit = character.getNumericValue(cardNumber.charAt(i));
+		for (int i = cardNumber.length() - 1; i > 0; i--){	
+			int digit = Character.getNumericValue(cardNumber.charAt(i));
 
 			if(isEvenPosition){
 				digit *= 2;
@@ -30,47 +30,47 @@ public class CreditCardValidator{
 		return (totalSum % 10) == 0;
 	}
 
-		public static String getCardType(String cardNumber){
-			if(cardNumber.startsWith("4")){
-				return "visa";
-			} else if (cardNumber.startsWith("5")){
-				return "masterCard";
-			} else if (cardNumber.startswuth("37")){
-				return "American Express";
-			} else if (cardNumber.startsWith("6")){
-				return "Discovery";
-			} else{
-				return "Invalid: Unknown Card Type";
-			}
+	public static String getCardType(String cardNumber){
+		if(cardNumber.startsWith("4")){
+			return "visa";
+		} else if (cardNumber.startsWith("5")){
+			return "masterCard";
+		} else if (cardNumber.startsWith("37")){
+			return "American Express";
+		} else if (cardNumber.startsWith("6")){
+			return "Discovery";
+		} else{
+			return "Invalid: Unknown Card Type";
 		}
+	}
 
 
-		public static void main(String[] args){
-			ArrayList<String> cardDetails = new ArrayList<String>();
-			Scanner input = new Scanner(System.in);
+	public static void main(String[] args){
+		ArrayList<String> cardDetails = new ArrayList<>();
+		Scanner input = new Scanner(System.in);
 		
-			System.out.println("Hello, Kindky Enter Card Details To Verify");
-			System.out.println("Enter Credit Card Number:  ");
-			String cardNumber = input.nextLine().trim();
+		System.out.println("Hello, Kindky Enter Card Details To Verify");
+		System.out.println("Enter Credit Card Number:  ");
+		String cardNumber = input.next().trim();
 
-			boolean isCardValid = validateCardNumber(cardNumber);
-			if(isCardValid){
-				String cardType = getCreditCard(cardNumber);
-				int cardLength = cardNumber.length();
-				String cardStatus ="Valid";
+		boolean isCardValid = validateCardNumber(cardNumber);
+		if(isCardValid){
+			String cardType = getCardType(cardNumber);
+			int cardLength = cardNumber.length();
+			String cardStatus = "Valid";
 
-				cardDetails.add("Credit Card Type: " + cardType);
-				cardDetails.add("Credit Card Number: " + cardNumber);
-				cardDetails.add("Credit Card DigitLength: " + diditLength);
-				cardDetails.add("Credit Card Validity Status: " +cardStatus);
-			} else{
-				cardDetails.add("Invalid Credit Card Number");
-			}
-
-			System.out.println("********************************************************");
-			for (String detail : cardDetails) {
-				System.out.println("**" + detail);
-			}
-			System.out.println("********************************************************");
+			cardDetails.add("Credit Card Type: " + cardType);
+			cardDetails.add("Credit Card Number: " + cardNumber);
+			cardDetails.add("Credit Card Card Length: " + cardLength);
+			cardDetails.add("Credit Card Validity Status: " + cardStatus);
+		} else{
+			cardDetails.add("Invalid Credit Card Number");
 		}
+
+		System.out.println("********************************************************");
+		for (String detail : cardDetails) {
+			System.out.println("**" + detail);
+		}
+		System.out.println("********************************************************");
+	}
 }
